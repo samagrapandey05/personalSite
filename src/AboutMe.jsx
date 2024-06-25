1de372138c7a23b714e98971e61381dffc9dc2e1
Samagra Pandey <samagrapandey@Samagras-MacBook-Air.local>
2024-06-24 17:18:01 -0700

    Modified Title 2

--- src/AboutMe.jsx
+++ src/AboutMe.jsx
@@ -13,8 +13,8 @@ function AboutMe(props) {
             <img src = {image} style ={{aspectRatio: "inherit", maxHeight: "50%", maxWidth: "30%", marginRight: "10%"}}/>
             <div id = "Personal_Description">
                 <h1 id = "name">Samagra Pandey</h1>
-                <a href='https://www.linkedin.com/in/samagra-pandey-691a1822b/'>LinkedIn</a>
-                <a href='https://github.com/samagrapandey05'>Github</a>
+                <a href='https://www.linkedin.com/in/samagra-pandey-691a1822b/' target="_blank">LinkedIn</a>
+                <a href='https://github.com/samagrapandey05' target="_blank">Github</a>
                 <p>
                     Hello there, my name is Samagra Pandey. I'm an undergraduate computer science student at UCLA. I am passionate about building software tools to solve real-world issues, and about expanding the domain of our knowledge in cutting edge fields, from machine learning to quantum computing.
                 </p>
