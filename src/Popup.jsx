a99472f672d9f9d2ad280bd56a3c368e48a6e1bd
Samagra Pandey <samagrapandey@Samagras-MacBook-Air.local>
2024-07-05 03:17:17 -0700

    Added link to cadbot beta

--- src/Popup.jsx
+++ src/Popup.jsx
@@ -27,7 +27,7 @@ function Popup(props) {
         <div className = "popupCard" ref = {popupRef}>
             <img src={cad} className='popupImg'/>
             <h1 className="projectTitle">CADbot</h1>
-            <a>Click here to view CADbot Beta</a>
+            <a href='https://main--cadbot.netlify.app/' target="_blank">Click here to view CADbot Beta</a>
             <p>Revolutionize your CAD and 3D-printing experience with CADbot, your personal AI assistant for everything CAD-related. Use it to create printable design files in an instant by simply typing out or drawing your intended object, or save time by downloading and refining CADbot's returned file. However you decide to use it, your CAD experience will never be same.</p>
         </div> : 
         props.algoClicked ? 
